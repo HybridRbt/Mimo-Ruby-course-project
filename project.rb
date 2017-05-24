584dbb367fde429e0207bf711fc99d9565f5a7b7
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-05-24 16:31:20 -0500

    add string interpolation

--- project.rb
+++ project.rb
@@ -2,3 +2,7 @@ string_1 = "This is a string. "
 string_2 = "This is another string."
 combined_string = string_1 + string_2
 print combined_string
+
+string_3 = "PAM"
+string_3 = string_3.reverse
+another_string = "This is what's inside string_3: #{string_3}"
